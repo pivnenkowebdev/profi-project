03139382661ddce9981d218b084cb60fabe41d94
Anton Pivnenko <pivnenkowebdev@gmail.com>
2025-10-08 12:12:10 +0300

    refactor: add accordeon frok other project

--- src/js/accordeon.js
+++ src/js/accordeon.js
@@ -1,13 +1,68 @@
-document.querySelector("#accordeon").addEventListener("click", e => {
-    const header = e.target.closest("[data-accordeon-header]");
-    if (!header) return;
+const accordeonElement = document.querySelector("#accordeon");
+let allItems = null;
+const localKey = "accordeonStage";
+const defaultStageIndex = "0";
 
-    const item = header.closest("[data-accordeon-item]");
-    const body = item.querySelector("[data-accordeon-body]");
+const setBaseStage = () => {
+  const indexFromStage = getStageOfAccordeon(localKey);
 
-    item.classList.toggle("active");
-    
-    body.style.height = item.classList.contains("active")
-        ? body.scrollHeight + "px"
-        : 0;
-});
+  if (indexFromStage !== null) {
+    setAccordeonStyle(indexFromStage);
+  } else {
+    setAccordeonStyle(defaultStageIndex);
+    setStageOfAccordeon(localKey, defaultStageIndex);
+  }
+};
+
+const setAccordeonStyle = (currentIndex) => {
+  if (currentIndex !== null && currentIndex !== undefined) {
+    const isActive = allItems[currentIndex].classList.contains("active");
+
+    allItems.forEach((item) => item.classList.remove("active"));
+
+    if (!isActive) {
+      allItems[currentIndex].classList.add("active");
+      setStageOfAccordeon(localKey, currentIndex);
+    } else {
+      setStageOfAccordeon(localKey, null);
+      return;
+    }
+  }
+};
+
+const getStageOfAccordeon = (lsKey) => {
+  const stage = JSON.parse(localStorage.getItem(lsKey));
+  return stage !== null ? String(stage) : null;
+};
+
+const setStageOfAccordeon = (lsKey, lsValue) => {
+  if (lsKey) {
+    localStorage.setItem(lsKey, JSON.stringify(lsValue));
+  }
+};
+
+const getIndexCurrentElement = (element) => {
+  const currentIndex = Array.from(allItems).indexOf(element);
+  return String(currentIndex);
+};
+
+const eventHandler = (e) => {
+  const target = e.target;
+  if (target) {
+    const headerItemElement = target.closest("[data-header-item]");
+    if (headerItemElement) {
+      const baseElement = headerItemElement.closest("[data-base]");
+      const currentElementIndex = getIndexCurrentElement(baseElement);
+      if (currentElementIndex !== null) {
+        setStageOfAccordeon(localKey, currentElementIndex);
+        setAccordeonStyle(currentElementIndex);
+      }
+    }
+  }
+};
+
+if (accordeonElement) {
+  accordeonElement.addEventListener("click", (e) => eventHandler(e));
+  allItems = accordeonElement.querySelectorAll("[data-base]");
+  setBaseStage();
+}
